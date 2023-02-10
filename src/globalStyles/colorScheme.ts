268e503ee3e12f4ac0fd62680737ad1f20a325b6
bsk94 <annabiskup07@gmail.com>
2023-02-10 09:37:26 +0100

    styled components installed, global styles applied

---
+++ src/globalStyles/colorScheme.ts
@@ -0,0 +1,17 @@
+export const theme = {
+  colors: {
+    lightest: '#F9FFF7',
+    lightGreen: '#EFF5EC',
+    midGreen: '#CED4CD',
+    darkGreen: '#8B928A',
+    black: '#000000',
+    white: '#ffffff',
+    neonGreen: '#C7FFBC',
+    neonOrange: '#FF817F',
+    blue: '#9ADCFF',
+    yellow: '#FFF56D',
+    orange: '#FDB827',
+    pink: '#FF8AAE',
+    green: '#C6D57E'
+  }
+};
---
+++ src/globalStyles/globalStyle.ts
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from 'styled-components';
+import Electrolize from '../assets/fonts/Electrolize-Regular.ttf';
+import styled from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+   * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        text-decoration: none;
+    }
+
+    button {
+         cursor: pointer;
+         font-family: "Electrolize-Regular";
+    }
+
+    ul {
+         list-style: none
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    html { 
+        font-size: 62.5%;
+         @media screen and (max-width: 600px){
+        font-size: 50%;
+        }
+    }  
+
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+
+    /* Firefox */
+    input[type='number'] {
+      -moz-appearance: textfield;
+    }
+
+    @font-face {
+    font-family: 'Electrolize-Regular';
+    src: url(${Electrolize}) format('truetype');
+    font-weight: 300;
+    font-style: normal;
+    font-display: auto;
+  }
+
+
+`;
