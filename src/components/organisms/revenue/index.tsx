bc7808e876b304edb227fa5572e9851c19f68a6b
bsk94 <annabiskup07@gmail.com>
2023-02-12 09:32:16 +0100

    revenue-basic form structure added

--- src/components/organisms/revenue/index.tsx
+++ src/components/organisms/revenue/index.tsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import { Formik, Form } from 'formik';
+import Input from '../../atoms/input';
+import Button from '../../atoms/button';
 
 const AddRevenue = () => {
-  return <div>Revenue</div>;
+  const initialValues = {
+    name: '',
+    date: '',
+    amount: ''
+  };
+  return (
+    <>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values) => {
+          console.log(values);
+        }}>
+        {({ values, handleChange }) => (
+          <>
+            <Form>
+              <h2>+ Add revenue</h2>
+              <Input
+                type="text"
+                name="name"
+                onChange={handleChange}
+                value={values.name}
+                placeholder=" ">
+                name
+              </Input>
+
+              <Input
+                type="text"
+                name="date"
+                onChange={handleChange}
+                value={values.date}
+                placeholder=" ">
+                date
+              </Input>
+
+              <Input
+                type="number"
+                name="amount"
+                onChange={handleChange}
+                value={values.amount}
+                placeholder=" ">
+                amount
+              </Input>
+
+              <Button type="submit">Add</Button>
+            </Form>
+          </>
+        )}
+      </Formik>
+    </>
+  );
 };
 
 export default AddRevenue;
