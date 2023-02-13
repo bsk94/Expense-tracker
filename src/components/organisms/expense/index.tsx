26426bdfa9bf5b40a53bf845b1b5a555c4b42b75
bsk94 <annabiskup07@gmail.com>
2023-02-13 11:27:46 +0100

    basic expense form added

--- src/components/organisms/expense/index.tsx
+++ src/components/organisms/expense/index.tsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import Button from '../../atoms/button';
+import Input from '../../atoms/input';
+import FormError from '../../atoms/formError';
+import { Formik } from 'formik';
+import { StyledForm } from '../../../shared/styles';
+import { addRevExpValidationSchema } from '../../../validation/financeFormSchema';
 
 const AddExpense = () => {
-  return <div>Expense</div>;
+  const initialValues = {
+    name: '',
+    date: '',
+    amount: ''
+  };
+  return (
+    <>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={addRevExpValidationSchema}
+        onSubmit={(values) => {
+          console.log(values);
+        }}>
+        {({ values, handleChange }) => (
+          <>
+            <StyledForm>
+              <h2>+ Add revenue</h2>
+              <Input
+                type="text"
+                name="name"
+                onChange={handleChange}
+                value={values.name}
+                placeholder=" ">
+                name
+              </Input>
+              <FormError name="name" />
+              <Input
+                type="date"
+                name="date"
+                onChange={handleChange}
+                value={values.date}
+                placeholder=" ">
+                date
+              </Input>
+              <FormError name="date" />
+              <Input
+                type="number"
+                name="amount"
+                onChange={handleChange}
+                value={values.amount}
+                placeholder=" ">
+                amount
+              </Input>
+              <FormError name="amount" />
+              <Button type="submit" className="formRev__btn--add">
+                Add
+              </Button>
+            </StyledForm>
+          </>
+        )}
+      </Formik>
+    </>
+  );
 };
 
 export default AddExpense;
