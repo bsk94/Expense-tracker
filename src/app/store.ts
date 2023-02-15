e29c2b79c93f23567d72ff9f47b66aefdaf8ed90
bsk94 <annabiskup07@gmail.com>
2023-02-15 11:28:48 +0100

    filterByFinanceType- active class added

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import financesSlice from '../features/financeSlice';
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    finance: financesSlice
+  }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/components/molecules/filterByFinanceType/index.tsx
+++ src/components/molecules/filterByFinanceType/index.tsx
@@ -1,11 +1,22 @@
 import { StyledList } from './filterByFinanceType-styles';
 import { financeType } from '../../../helpers/category';
+import { useDispatch, useSelector } from 'react-redux';
+import type { RootState } from '../../../app/store';
+import { currentFinanceCategory } from '../../../features/financeSlice';
 
 const FilterByFinanceType = () => {
+  const dispatch = useDispatch();
+  const currentFinType = useSelector((state: RootState) => state.finance.financeTypes);
+
   return (
     <StyledList>
       {financeType.map((category) => (
-        <li key={category}>{category}</li>
+        <li
+          className={currentFinType === category ? 'active' : ''}
+          onClick={() => dispatch(currentFinanceCategory(category))}
+          key={category}>
+          {category}
+        </li>
       ))}
     </StyledList>
   );
