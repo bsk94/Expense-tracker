c39a071cf9dacad85a85ce956ec0c25fa1339947
bsk94 <annabiskup07@gmail.com>
2023-02-22 19:53:21 +0100

    statistics-styling added

--- src/components/organisms/statistics/index.tsx
+++ src/components/organisms/statistics/index.tsx
@@ -1,8 +1,21 @@
+import { color } from 'd3-color';
 import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { theme } from '../../../globalStyles/colorScheme';
 import { StyledContainer } from './statistics-styles';
+import { useIsDesktop } from '../../../shared/hooks/isDesktop';
+import {
+  StyledIconListItem,
+  StyledIconList,
+  StyledTextContainer,
+  StyledCalendarIcon
+} from './statistics-styles';
+import { useState } from 'react';
+import SvgIcon from '../../atoms/svg';
+import { categoryForm } from '../../../helpers/category';
+import DatePickerModal from '../../atoms/datePicker';
 
 const Statistics = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const data = [
     { name: 'Home', value: 400, color: theme.colors.blue },
     { name: 'Food', value: 200, color: theme.colors.pink },
@@ -11,19 +24,50 @@ const Statistics = () => {
     { name: 'Other', value: 700, color: theme.colors.green }
   ];
 
-  return (
-    <ResponsiveContainer width="95%" height={400}>
-      <PieChart data={data}>
-        <Pie dataKey="value" data={data} cx="50%" cy="40%" innerRadius={60} stroke="none">
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={entry.color} />
-          ))}
-        </Pie>
-        <Tooltip />
+  const { isDesktop } = useIsDesktop();
 
-        <Legend iconType={'rect'} align={'right'} verticalAlign={'top'} layout={'vertical'} />
-      </PieChart>
-    </ResponsiveContainer>
+  return (
+    <>
+      <DatePickerModal
+        selectRange={true}
+        onChange={(e: any) => console.log(e)}
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+      />
+      <StyledContainer>
+        <ResponsiveContainer height="55%" width="100%">
+          <PieChart data={data}>
+            <Pie dataKey="value" data={data} cx="50%" cy="55%" innerRadius={60} stroke="none">
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={entry.color} />
+              ))}
+            </Pie>
+            <Tooltip />
+
+            <Legend
+              iconType={'rect'}
+              align={isDesktop ? 'right' : 'center'}
+              verticalAlign={'top'}
+              layout={isDesktop ? 'vertical' : 'horizontal'}
+            />
+          </PieChart>
+        </ResponsiveContainer>{' '}
+        <StyledIconList>
+          <StyledCalendarIcon onClick={() => setIsModalOpen(true)} alt="calendar icon" />
+          {categoryForm.map(({ cat, icon }) => {
+            return (
+              <StyledIconListItem key={cat}>
+                <SvgIcon Icon={icon} width={isDesktop ? 55 : 42} />
+                <StyledTextContainer>
+                  <span>{cat}</span>
+                  <span>2000$</span>
+                </StyledTextContainer>
+              </StyledIconListItem>
+            );
+          })}
+        </StyledIconList>
+      </StyledContainer>
+    </>
   );
 };
 
--- src/components/organisms/statistics/statistics-styles.ts
+++ src/components/organisms/statistics/statistics-styles.ts
@@ -1,6 +1,56 @@
 import styled from 'styled-components';
+import { media } from '../../../globalStyles/mediaQueries';
+import CalendarIcon from '../../../assets/icons/calendar-line.svg';
 
 export const StyledContainer = styled.div`
-  height: 20vh;
-  width: 20%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* justify-content: center; */
+  height: 100%;
+  width: 100%;
+  ${media.mobile} {
+    background-color: ${({ theme }) => theme.colors.lightest};
+  }
+  .recharts-responsive-container {
+  }
+
+  .recharts-legend-wrapper {
+  }
+
+  .recharts-default-legend {
+  }
+
+  .recharts-legend-item-text {
+    color: black !important;
+  }
+`;
+
+export const StyledIconList = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  ${media.desktop} {
+    column-gap: 10rem;
+  }
+`;
+
+export const StyledIconListItem = styled.li`
+  display: flex;
+  flex-direction: row;
+  margin: 1rem;
+`;
+
+export const StyledTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 1rem;
+`;
+
+export const StyledCalendarIcon = styled.img.attrs({
+  src: `${CalendarIcon}`
+})`
+  align-self: flex-start;
+  grid-column: 1 / span 2;
+  padding: 1.5rem;
 `;
