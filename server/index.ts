ebf7054a11f93091acf3ba727a5bffd6859f05c0
bsk94 <annabiskup07@gmail.com>
2023-03-05 14:35:07 +0100

    connected to database, mvp model folder structure

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 4000;
 const serv = require('http').createServer(app);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+dotenv.config();
+
+app.listen(port, async () => {
+  const db = await mongoose.connect(`${process.env.DATABASE_CONNECTION}`);
+
+  console.log(`Server is running ${port}`);
 });
